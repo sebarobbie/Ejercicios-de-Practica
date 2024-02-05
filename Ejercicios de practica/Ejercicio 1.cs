87f4fa2298557b93c5457981c374ea8d758114f5
Sebastian Francisco <76788336+sebarobbie@users.noreply.github.com>
2024-02-05 15:15:47 -0400

    Add files via upload

---
+++ Ejercicios de practica/Ejercicio 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Hacer que el sujeto ponga dos digitos
+        Console.Write("Ingrese numero de dos digitos: ");
+        string input = Console.ReadLine();
+
+        // Chequear si son mas de dos digitos
+        if (input.Length != 2)
+        {
+            Console.WriteLine("Por favor, ingrese un numero de dos dígitos.");
+            return;
+        }
+
+        // Hacer que sea un numero entero
+        int numero = int.Parse(input);
+
+        // Calculo de la suma con resultado
+        Console.WriteLine($"La suma de los dígitos es: {numero / 10 + numero % 10}");
+
+    }
+}
